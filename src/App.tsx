ae8f38f8ca0bcf86fb48480d53a478351cd17092
Miguel Cortez <mickeycortez2002@gmail.com>
2026-04-09 19:45:18 -0600

    feat: Header terminado

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import SplashScreen from './SplashScreen'
 import Confetti from './components/Confetti'
 import Stars from './components/Stars'
+import Header from './components/header/header'
 import './App.css'
 
 function App() {
@@ -14,9 +15,14 @@ function App() {
 
       {showSplash && <SplashScreen onDone={() => setShowSplash(false)} />}
 
-      <div>
-        <h2>¡Feliz cumpleaños, Emi! 🎉</h2>
-      </div>
+      {!showSplash && (
+        <>
+          <Header />
+          <div>
+            <h2>¡Feliz cumpleaños, Emi! 🎉</h2>
+          </div>
+        </>
+      )}
     </>
   )
 }
